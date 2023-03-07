6c405864bb943d37dbdaf08403325cd2f83dd118
blakesmithmn <blake.smith.mn@gmail.com>
2023-03-07 17:12:34 -0600

    wrote code that appends to an existing file - and also some others that replaces existing text content

--- Start/Files/ReadWriteFiles/Program.cs
+++ Start/Files/ReadWriteFiles/Program.cs
@@ -6,14 +6,20 @@ const string filename = "TestFile.txt";
 
 // TODO 1: WriteAllText overwrites a file with the given content
 if (!File.Exists(filename)) {
-
+    File.WriteAllText(filename, "This is a text file."); // OVERWRITES EXISTING CONTENT
 }
 
 // TODO 3: AppendAllText adds text to an existing file
-
+File.AppendAllText(filename, "This text gets appended to the file.");
 
 // TODO 4: A FileStream can be opened and written to until closed
-
+using (StreamWriter sw = File.AppendText(filename)) {
+    sw.WriteLine("Line One");
+    sw.WriteLine("Line Two");
+    sw.WriteLine("Line Three");
+}
 
 // TODO 2: ReadAllText reads all the content from a file
-
+string content;
+content = File.ReadAllText(filename);
+Console.WriteLine(content);
