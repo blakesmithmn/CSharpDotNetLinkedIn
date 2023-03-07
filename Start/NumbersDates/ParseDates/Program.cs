70dd936b5c1c6022c47367c9a518b94ffbd0e1d2
blakesmithmn <blake.smith.mn@gmail.com>
2023-03-07 16:24:11 -0600

    overview of parshing dates using built in parsing and loops

--- Start/NumbersDates/ParseDates/Program.cs
+++ Start/NumbersDates/ParseDates/Program.cs
@@ -15,4 +15,19 @@ string[] sampleDateTimes = {
 foreach (string datestr in sampleDateTimes) {
     DateTime result;
     // TODO: Use the static class function TryParse to try parsing the dates
+
+    if(DateTime.TryParse(datestr, out result)){
+        Console.WriteLine($"{datestr, -25}, gets parsed as: {result}");
+    }
+    else {
+        Console.WriteLine($"Could not parse '{datestr}");
+    }
 }
+
+// January 1, 2025 9:30 AM  , gets parsed as: 1/1/2025 9:30:00 AM
+// 1/1/2025                 , gets parsed as: 1/1/2025 12:00:00 AM
+// Jan 1, 2025 7:30PM       , gets parsed as: 1/1/2025 7:30:00 PM
+// Jan 1, 25                , gets parsed as: 1/1/2025 12:00:00 AM
+// 1/2025                   , gets parsed as: 1/1/2025 12:00:00 AM
+// 1/1 7PM                  , gets parsed as: 1/1/2023 7:00:00 PM
+// Could not parse 'Jan 1 '15
