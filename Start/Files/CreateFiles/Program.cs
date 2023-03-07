a886d807ff249e284e845240b39772ec2be72452
blakesmithmn <blake.smith.mn@gmail.com>
2023-03-07 16:58:06 -0600

    learned how to start to manipulate files using csharp - including adding / deleting files if they exist

--- Start/Files/CreateFiles/Program.cs
+++ Start/Files/CreateFiles/Program.cs
@@ -5,6 +5,22 @@ const string filename = "TestFile.txt";
 
 // TODO: Create a new file - this will overwrite any existing file
 // Use the "using" construct to automatically close the file stream
-
+// using(StreamWriter sw = File.CreateText(filename)){
+//     sw.WriteLine("This is a text file we made using C sharp ! Kinda neat huh");
+// };
 
 // TODO: Determine if a file exists
+// checks if file exists and prints to console
+Console.WriteLine(File.Exists(filename));
+if(File.Exists(filename)){
+    // if file exists delete it
+    File.Delete(filename);
+}
+else {
+    // else create a new file using this function
+    using(StreamWriter sw = File.CreateText(filename)){
+        sw.WriteLine("This is a text file we made using C sharp ! Kinda neat huh");
+    };
+}
+Console.WriteLine(File.Exists(filename));
+
