833abd3648f88027a309a4efb6639aaa5ec8aea9
blakesmithmn <blake.smith.mn@gmail.com>
2023-03-07 12:33:24 -0600

    exercise two went over combining data types, as well as dealing with refernces vs value types

--- Start/Overview/DataTypes/Program.cs
+++ Start/Overview/DataTypes/Program.cs
@@ -4,18 +4,31 @@
 // Declare some types with values
 int a = 1;
 char c = 'A';
-float f = 123.45f;
+float f = 123.45f; 
 decimal d = 400.85m;
-int b = default;
-bool tf = default;
+int b = default; // default 0
+bool tf = default; // default false
 
 Console.WriteLine($"{a}, {b}, {tf}, {c}, {f}, {d}");
 
 // TODO: implicit type conversion
+    // character + integer
+    Console.WriteLine($"{c+a}");
+    // results in character getting converted to a number
+    Console.WriteLine($"{(char)(c + a)}"); // casting it to a character
+    // returns 'B' 
+
+    // Adding a floating point to an integer
+    Console.WriteLine($"{f + a}"); // 124.45
+
+    // Adding a floating point to a character
+    Console.WriteLine($"{f + c}"); // 188.45
 
 
 // TODO: Create an instance of a struct (which is a value type)
-
+s s1;
+s1.a = 5;
+s1.b = false;
 
 // Perform an operation on a struct
 void StructOp(s theStruct) {
@@ -25,12 +38,13 @@ void StructOp(s theStruct) {
     Console.WriteLine($"{theStruct.a}, {theStruct.b}");
 }
 
-// Console.WriteLine("Structs are passed by copy, since they are value types:");
-// Console.WriteLine($"{s1.a}, {s1.b}");
-// StructOp(s1);
-// Console.WriteLine($"{s1.a}, {s1.b}");
+Console.WriteLine("Structs are passed by copy, since they are value types:");
+Console.WriteLine($"{s1.a}, {s1.b}"); // original variables
+StructOp(s1); // changed a COPY of the variables within the function
+Console.WriteLine($"{s1.a}, {s1.b}"); // back to original values bc structs are value types
 
 // TODO: Create an object instance of a class (which is a reference type)
+MyClass cl = new MyClass{a=5, b=false};
 
 
 // Perform an operation on the class
@@ -41,18 +55,20 @@ void ClassOp(MyClass theClass) {
     Console.WriteLine($"{theClass.a}, {theClass.b}");
 }
 
-// Console.WriteLine("Objects are passed by reference, since they are reference types:");
-// Console.WriteLine($"{cl.a}, {cl.b}");
-// ClassOp(cl);
-// Console.WriteLine($"{cl.a}, {cl.b}");
+Console.WriteLine("Objects are passed by reference, since they are reference types:");
+Console.WriteLine($"{cl.a}, {cl.b}"); // 5 true
+ClassOp(cl); // 10 true
+Console.WriteLine($"{cl.a}, {cl.b}"); // 10 true - because after the function completes - the values are changed bc it's a class REFERENCE TYPES
 
 // These are declared at the bottom of the file because C# requires
 // top-level statements to come before type declarations
+// class is a reference type
 class MyClass {
     public int a;
     public bool b;
 }
 
+// struct is a value type
 struct s {
     public int a;
     public bool b;
