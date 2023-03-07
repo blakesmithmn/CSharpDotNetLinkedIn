df9b62a2192c4afe1f7307a6693f7f4b0662baae
blakesmithmn <blake.smith.mn@gmail.com>
2023-03-07 13:15:00 -0600

    able to search through strings and compare them using built in dotnet functions!

--- Start/Strings/Searching/Program.cs
+++ Start/Strings/Searching/Program.cs
@@ -4,16 +4,33 @@
 string teststr = "The quick brown Fox jumps over the lazy Dog";
 
 // TODO: Contains determines whether a string contains certain content
+Console.WriteLine($"{teststr.Contains("fox")}"); // takes casing into account to this would be false
+Console.WriteLine($"{teststr.Contains("fox", StringComparison.CurrentCultureIgnoreCase)}"); // would return true after ignoring case
 
 
 // TODO: StartsWith and EndsWith determine if a string starts 
 // or ends with a given test string
+Console.WriteLine($"{teststr.StartsWith("the")}"); // false
+Console.WriteLine($"{teststr.StartsWith("the", StringComparison.CurrentCultureIgnoreCase)}"); // true
+
+Console.WriteLine($"{teststr.EndsWith("dog")}"); // false 
+Console.WriteLine($"{teststr.EndsWith("dog", StringComparison.CurrentCultureIgnoreCase )}"); // true
 
 
 // TODO: IndexOf, LastIndexOf finds the index of a substring
+// -1 if it isn't found
+Console.WriteLine($"{teststr.IndexOf("the")}"); // 31
+Console.WriteLine($"{teststr.IndexOf("the", StringComparison.CurrentCultureIgnoreCase)}"); // 0
+
+Console.WriteLine($"{teststr.LastIndexOf("the")}"); //  31
+
 
 
 // TODO: Determining empty, null, or whitespace
 string str1 = null;
 string str2 = "   ";
 string str3 = String.Empty;
+
+Console.WriteLine($"{String.IsNullOrEmpty(str1)}"); // true
+Console.WriteLine($"{String.IsNullOrEmpty(str3)}"); // true
+Console.WriteLine($"{String.IsNullOrWhiteSpace(str2)}"); // true
